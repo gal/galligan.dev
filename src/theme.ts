36b307e4232c58dad35a1a02efb4e397bdb4b88f
Thomas Daniel Galligan <thomas007g@gmail.com>
2021-12-31 15:55:03 +0000

    removed unnecessary error theme color

--- src/theme.ts
+++ src/theme.ts
@@ -1,5 +1,4 @@
 import { createTheme } from '@mui/material/styles';
-import { red } from '@mui/material/colors';
 
 // Create a theme instance.
 const theme = createTheme({
@@ -10,9 +9,6 @@ const theme = createTheme({
     },
     secondary: {
       main: '#252525',
-    },
-    error: {
-      main: red.A400,
     },
     success: {
       main: '#fff'
