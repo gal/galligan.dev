21d527c05ae43c1daf6d70fda9c56f076a66db68
Thomas Daniel Galligan <thomas007g@gmail.com>
2023-05-27 21:51:36 +0100

    added gallery

---
+++ gallery.js
@@ -0,0 +1,51 @@
+var gallery = document.getElementById("gallery");
+var image_node = document.getElementById("img_container_node");
+const modalContainer = document.getElementById("modal_container");
+const modal = document.getElementById("modal");
+const modalImg = document.getElementById("modal_img");
+
+modalContainer.addEventListener("click", () => {
+  modalContainer.style.display = "none";
+});
+
+const imageClicked = (image) => {
+  // when image is clicked, open up a larger view of the image in a modal-type view
+  console.log(image)
+
+  modalContainer.style.display = "block";
+  modalImg.src = image.src;
+  modalImg.alt = image.alt;
+}
+
+const getImages = async () => {
+  console.log("Downloading manifest")
+  const res = await fetch("/manifest.json");
+  const data = await res.json();
+  console.log("Manifest downloaded")
+  console.log(data)
+  const domain = data.domain;
+  const images = data.images;
+
+  images.forEach((image) => {
+    const newImgContainer = image_node.cloneNode(true);
+    newImgContainer.id = "";
+    const newImg = newImgContainer.querySelector("img");
+    
+    const imageSplit = image.url.split(".");
+    if (imageSplit.length > 2) {
+      newImg.src = image.url
+    } else {
+      newImg.src = "https://" + domain + "/preview/" + image.url
+    }
+
+    newImg.alt = image.alt;
+
+    gallery.appendChild(newImgContainer);
+
+    newImg.addEventListener("click", () => {
+      imageClicked(newImg);
+    });
+  });
+}
+
+getImages()
