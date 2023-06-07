feb0c791a3ea31ddc98b23f1bd710ddb0721c6bf
Thomas Daniel Galligan <thomas007g@gmail.com>
2023-06-07 20:43:14 +0100

    remove use-client from home

--- app/page.tsx
+++ app/page.tsx
@@ -1,30 +1,3 @@
-"use client"
-
-import { useLayoutEffect } from "react"
-
-import { redirect } from "next/navigation"
-// import { Metadata } from "next"
-
-// export const metadata: Metadata = {
-//   title: "About Thomas Daniel Galligan",
-//   description: "Thomas Daniel Galligan is a software engineer based in Ireland.",
-//   openGraph: {
-//     type: "profile",
-//     countryName: "Republic of Ireland",
-//     description: "Thomas Daniel Galligan is a software engineer based in Ireland.",
-//     locale: "en_IE",
-//     alternateLocale: "en_US",
-//     url: "https://gal.ie/about",
-//     emails: [
-//       "thomas@galligan.dev",
-//     ],
-//     firstName: "Thomas",
-//     lastName: "Galligan",
-//     gender: "male",
-//     username: "gal | thomas007g",
-//   },
-// }
-
 export default function Home() {
   return (<></>)
 }
