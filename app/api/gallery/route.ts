25a31ecbb73dd35d0837d2a22c2b5143d3fd7728
Thomas Daniel Galligan <thomas007g@gmail.com>
2023-09-23 12:30:48 +0100

    remove api routes

--- app/api/gallery/route.ts
+++
@@ -1,7 +0,0 @@
-import manifest from "./manifest.json";
-
-export async function GET(request: Request) {
-  return new Response(JSON.stringify(manifest), {
-    headers: { "Content-Type": "application/json" },
-  });
-}
