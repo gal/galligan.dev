568510374d92baa95830fc0c517e3c5773bac21f
Thomas Daniel Galligan <thomas007g@gmail.com>
2024-10-04 12:46:30 +0100

    css fixes

--- components/main/Header.tsx
+++ components/main/Header.tsx
@@ -1,13 +1,17 @@
 const Header = () => {
     return (
-        <header className="z-50 m-0 w-[100dvw] header h-[10dvh] md:grid md:grid-cols-5 sticky">
-            <div></div>
-            <div></div>
-            <nav className="h-full w-full md:col-span-3 flex flex-row justify-center md:justify-start items-center text-xl">
-                <a href="/">Home</a>
-                <a href="https://blog.galligan.dev">Blog</a>
-                <a href="/photography">Photography</a>
-            </nav>
+        <header className="z-50 px-[3em] my-0  w-[100dvw] header h-[10dvh]">
+            <div className="h-full mx-auto max-w-[1280px] md:grid md:grid-cols-5 sticky">
+
+                <div></div>
+                <div></div>
+                <nav className="h-full max-w-[1280px] md:col-span-3 flex flex-row justify-center md:justify-start items-center text-xl">
+                    <a href="/">Home</a>
+                    <a href="https://blog.galligan.dev">Blog</a>
+                    <a href="/photography">Photography</a>
+                </nav>
+            </div>
+
         </header>
     )
 }
